10f4b7016c15ec31e2f4100c6f82f8dff36bdb41
Le Chen <LeChen11111@163.com>
2024-11-01 21:08:55 +0800

    add utils/, core.js, index.js

---
+++ src/core.js
@@ -0,0 +1,10 @@
+import pull from "./api/array/pull.js";
+
+export default class Core {
+    constructor() {
+    }
+}
+
+Core.prototype.pull = pull;
+
+
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Core from "./core.js";
+
+const _ = new Core();
+export default _;
+
