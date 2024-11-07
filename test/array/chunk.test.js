eea5035c1659c9c3489bcd51df8907f31126202c
Le Chen <LeChen11111@163.com>
2024-11-07 16:59:27 +0800

    Adding a test category

--- test/array/chunk.test.js
+++ test/array/chunk.test.js
@@ -1,6 +1,9 @@
 import chunk from "../../src/api/array/chunk.js";
 import lodashStable from "lodash";
 const falsey = [, null, undefined, false, 0, NaN, ""];
+const stubArray = function () {
+  return [];
+};
 
 describe("chunk", function() {
     const array = [1, 2, 3, 4, 5, 6];
@@ -23,11 +26,35 @@ describe("chunk", function() {
 
     it("should treat falsey `size` values, except `undefined`, as `0`", function() {
         const expected = lodashStable.map(falsey, function (value) {
-          return value === undefined ? [[0], [1], [2], [3], [4], [5]] : [];
+          return value === undefined ? [[1], [2], [3], [4], [5], [6]] : [];
         });
         const actual = lodashStable.map(falsey, function (size, index) {
           return index ? chunk(array, size) : chunk(array);
         });
         expect(actual).toStrictEqual(expected);
+    });
+
+    it("should ensure the minimum `size` is `0`", function () {
+      const values = lodashStable
+        .reject(falsey, lodashStable.isUndefined)
+        .concat(-1, -Infinity);
+      const expected = lodashStable.map(values, stubArray);
+  
+      const actual = lodashStable.map(values, function (n) {
+        return chunk(array, n);
+      });
+  
+      expect(actual).toStrictEqual(expected);
+    });
+  
+    it("should coerce `size` to an integer", function () {
+      expect(chunk(array, array.length / 4)).toStrictEqual([
+        [1],
+        [2],
+        [3],
+        [4],
+        [5],
+        [6]
+      ]);
     });
 });
