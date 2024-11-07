db9b446874cabd8c5ca5de53c740be9da641012c
Le Chen <LeChen11111@163.com>
2024-11-07 10:19:26 +0800

    add property drop

--- src/core.js
+++ src/core.js
@@ -9,5 +9,6 @@ export default class Core {
 Core.prototype.pull = ArrayList.pull;
 Core.prototype.chunk = ArrayList.chunk;
 Core.prototype.compact = ArrayList.compact;
+Core.prototype.drop = ArrayList.drop;
 
 
