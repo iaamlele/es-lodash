3fa363ca7ae1866ebe0f2650fdab514b6e3c9400
Le Chen <LeChen11111@163.com>
2024-11-07 09:25:43 +0800

    add property chunk

--- src/core.js
+++ src/core.js
@@ -1,10 +1,12 @@
-import pull from "./api/array/pull.js";
+import ArrayList from "./api/array/index.js";
 
 export default class Core {
     constructor() {
     }
+
 }
 
-Core.prototype.pull = pull;
+Core.prototype.pull = ArrayList.pull;
+Core.prototype.chunk = ArrayList.chunk;
 
 
