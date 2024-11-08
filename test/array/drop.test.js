5dba7b6f7a519b964a8540c8ea2108655524d005
Le Chen <LeChen11111@163.com>
2024-11-08 10:30:09 +0800

    add test code of drop function

---
+++ test/array/drop.test.js
@@ -0,0 +1,38 @@
+import drop from "../../src/api/array/drop.js";
+import lodashStable, { values } from "lodash";
+const falsey = [, null, undefined, false, 0, NaN, ""];
+
+describe("drop", function() {
+    const array = [1, 2, 3];
+    it("should drop the first two elements", function() {
+        expect(drop(array, 2)).toStrictEqual([3]);
+    });
+
+    it("should treat falsey `n` values, except `undefined`, as `0`", function() {
+        const expected = lodashStable.map(falsey, function(value) {
+            return value === undefined ? [2, 3] : array;
+        })
+
+        const actual = lodashStable.map(falsey, function(n) {
+            return drop(array, n);
+        });
+
+        expect(actual).toStrictEqual(expected);
+    })
+
+    it("should return all elements when `n` < `1`", function() {
+        lodashStable.each([0, -1, -Infinity], function(n) {
+            expect(drop(array, n)).toStrictEqual(array);
+        })
+    })
+
+    it("should return an empty array when `n` >= `length`", function() {
+        lodashStable.each([4, 5, Infinity], function(n) {
+            expect(drop(array, n)).toStrictEqual([]);
+        })
+    })
+
+    it("should coerce `n` to an integer", function() {
+        expect(drop(array, 1.6)).toStrictEqual([2, 3]);
+    })
+})
